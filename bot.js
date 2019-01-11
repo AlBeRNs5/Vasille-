3adc4e1b5c503c36306d6f165890cd3647306808
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-12 01:50:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,25 +71,7 @@ client.on('message', message => {
     
     message.reply("بتمنشن ل بوت يا مان .؟ ");
 
- client.on('message', message => {
-    if (message.content.startsWith("invite")) {
  
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
  
  
  
